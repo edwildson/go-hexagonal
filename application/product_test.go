ad5f3cc52c59ffd10ea7b41c279212c86ddbbd54
Edwildson C. Rodrigues <edwildson@hotmail.com>
2023-08-08 04:05:21 -0300

    fix: fix test

--- application/product_test.go
+++ application/product_test.go
@@ -18,8 +18,8 @@ func TestProduct_Enable(t *testing.T)  {
 	require.Nil(t, err)
 
 	product.Price = 0
-
+	
 	err = product.Enable()
 
-	require.Equal(t, "T he price must be greater than zero to enable the product", err.Error())
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
 }
